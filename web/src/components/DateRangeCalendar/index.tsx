0d45f1095f27b89707a329e686dfc6745e0d7be4
Gean Lucas <gean.lfpsa@gmail.com>
2023-03-07 22:26:18 -0300

    feat: add range calendar and dates state

--- web/src/components/DateRangeCalendar/index.tsx
+++ web/src/components/DateRangeCalendar/index.tsx
@@ -1,5 +1,23 @@
+import { RangeCalendar } from "@adobe/react-spectrum";
+import { getLocalTimeZone, parseAbsolute } from "@internationalized/date";
+import { useState } from "react";
 import { Container } from "./styles";
 
 export function DateRangeCalendar() {
-  return <Container>Hello CSS!</Container>;
+  const [date, setDate] = useState({
+    start: parseAbsolute(new Date().toISOString(), getLocalTimeZone()),
+    end: parseAbsolute(new Date().toISOString(), getLocalTimeZone()),
+  });
+
+  return (
+    <Container>
+      <span>
+        Data inicial: <span>{date.start.toString()}</span>
+      </span>
+      <span>
+        Data final: <span>{date.end.toString()}</span>
+      </span>
+      <RangeCalendar value={date} onChange={setDate} visibleMonths={2} />
+    </Container>
+  );
 }
