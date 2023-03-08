3a8eb07bdfc05bc99dfb3f0eb81711ab5bbe54a0
Gean Lucas <gean.lfpsa@gmail.com>
2023-03-07 23:01:46 -0300

    feat: create range calendar styles

--- web/src/components/DateRangeCalendar/styles.ts
+++ web/src/components/DateRangeCalendar/styles.ts
@@ -5,4 +5,30 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 2rem;
+
+  table {
+    background: purple;
+    border-radius: 20px;
+  }
+
+  th {
+    span {
+      color: white;
+    }
+  }
+
+  [class*="is-range-selection"] {
+    span {
+      color: white;
+    }
+  }
+
+  [class*="is-selection-start"],
+  [class*="is-selection-end"] {
+    span {
+      color: cyan;
+      background: orange;
+      border-radius: 50%;
+    }
+  }
 `;
