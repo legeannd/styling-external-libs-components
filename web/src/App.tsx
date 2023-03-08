96293f7538305aa58c03eeef0739e2e64a00af3b
Gean Lucas <gean.lfpsa@gmail.com>
2023-03-07 21:52:22 -0300

    feat: create component files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,12 @@
+import { defaultTheme, Provider } from "@adobe/react-spectrum";
+import { DateRangeCalendar } from "./components/DateRangeCalendar";
+
 function App() {
-  return <h1>Hello CSS!</h1>;
+  return (
+    <Provider theme={defaultTheme} colorScheme="light">
+      <DateRangeCalendar></DateRangeCalendar>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ web/src/components/DateRangeCalendar/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function DateRangeCalendar() {
+  return <Container>Hello CSS!</Container>;
+}
